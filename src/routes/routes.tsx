f74623629fd5edb097179835ccd109f56cff291c
Pedro Souza <pedrosport10@gmail.com>
2024-07-31 10:12:28 -0300

    chore: ajust route to login

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,13 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "../components/Login/Login";
 import Header from "../components/Header/Header";
 import Home from "../pages/Home/Home";
+import LoginArea from "@/pages/LoginArea/LoginArea";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Header />,
     children: [
-      { path: "/login", element: <Login /> },
+      { path: "/login", element: <LoginArea /> },
       { path: "/home", element: <Home /> },
     ],
   },
