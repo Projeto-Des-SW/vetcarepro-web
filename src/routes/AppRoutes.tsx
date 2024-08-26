389aa543e3da86608c352199683fe10ff381bdc4
Pedro Souza <pedrosport10@gmail.com>
2024-08-26 20:54:26 -0300

    chore: improve routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -5,6 +5,10 @@ import Home from "@/pages/Home/Home";
 import LoginArea from "@/pages/LoginArea/LoginArea";
 import Pricing from "@/pages/Pricing/Pricing";
 import Dashboard from "@/pages/Dashboard/Dashboard";
+import CadastroClinica from "@/pages/CadastroClinica/CadastroClinica";
+import CadastrarPaciente from "@/pages/CadastrarPaciente/CadastrarPaciente";
+import ListagemPaciente from "@/pages/ListagemPaciente/ListagemPaciente";
+import ListagemClinica from "@/pages/ListagemClinica/ListagemClinica";
 
 const AppRoutes = () => {
   const user = useUserSelector((state) => state.user);
@@ -16,7 +20,22 @@ const AppRoutes = () => {
         <Route path="pricing" element={<Pricing />} />
         {user.email ? (
           <>
-            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="dashboard/listagemClinica" element={<Dashboard />}>
+              <Route path="cadastrarClinica" element={<CadastroClinica />} />
+              <Route path="cadastrarPaciente" element={<CadastrarPaciente />} />
+
+              <Route path="listagemClinica" element={<ListagemClinica />} />
+              <Route path="listagemPaciente" element={<ListagemPaciente />} />
+            </Route>
+
+            <Route
+              path="/editarPaciente/:id"
+              element={<CadastrarPaciente mode="edit" />}
+            />
+            <Route
+              path="/editarClinica/:id"
+              element={<CadastroClinica mode="edit" />}
+            />
             <Route path="*" element={<Navigate to="/dashboard" />} />
           </>
         ) : (
