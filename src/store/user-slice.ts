6afb422856b1c59ba24b1f78b9ca518984f2ab15
Pedro Souza <pedrosport10@gmail.com>
2024-09-04 11:10:15 -0300

    chore: adding some global managements to redux

--- src/store/user-slice.ts
+++ src/store/user-slice.ts
@@ -3,11 +3,19 @@ import { type PayloadAction, createSlice } from "@reduxjs/toolkit";
 export interface UserDataProps {
   email: string;
   token?: string;
+  notifications: INotifications[];
+}
+
+interface INotifications {
+  title: string;
+  description: string;
 }
 
 const userInicialState: UserDataProps = {
-  email: "",
-  token: "",
+  email: "admin@gmail.com",
+  token:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJmNjUwYWUyMy1kZjVkLTRhYTQtYThmNC1iMjhjNGE3MDQ4MWYiLCJpYXQiOjE3MjUxOTU5MzN9.suxA8pMjU8yvhGd4vWfuWPhrpEF6ZxG6CIWhmlWfIAw",
+  notifications: [{ title: "", description: "" }],
 };
 
 const userSlice = createSlice({
@@ -18,8 +26,23 @@ const userSlice = createSlice({
       state.email = action.payload.email;
       state.token = action.payload.token;
     },
+    logoutUser(state) {
+      state.email = "";
+      state.token = "";
+    },
+    addNotification(state, action: PayloadAction<INotifications>) {
+      state.notifications.push(action.payload);
+    },
+    clearNotifications(state) {
+      state.notifications = [];
+    },
   },
 });
 
 export default userSlice;
-export const { setCurrentUser } = userSlice.actions;
+export const {
+  setCurrentUser,
+  logoutUser,
+  addNotification,
+  clearNotifications,
+} = userSlice.actions;
