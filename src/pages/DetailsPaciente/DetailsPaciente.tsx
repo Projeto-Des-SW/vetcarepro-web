5eff701c963f6099e4d7229528c9924c714decf8
Pedro Souza <pedrosport10@gmail.com>
2024-09-09 00:22:56 -0300

    chore: improve UX to details

--- src/pages/DetailsPaciente/DetailsPaciente.tsx
+++ src/pages/DetailsPaciente/DetailsPaciente.tsx
@@ -1,18 +1,19 @@
-import { IPet, ITemp } from "@/interfaces/paciente";
+import { IPet } from "@/interfaces/paciente";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
-  CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useNavigate, useParams } from "react-router-dom";
 import { useUserSelector } from "@/store/hooks";
+import { Separator } from "@/components/ui/separator";
+import { fetchAgendamentosList } from "@/Services/GetServices";
+import dayjs from "dayjs";
 
 const DetailsPaciente = () => {
   const baseUrl = import.meta.env.VITE_URL as string;
@@ -37,11 +38,34 @@ const DetailsPaciente = () => {
     queryFn: fetchClinicasList,
   });
 
-  if (error) return <div>Error fetching clinics</div>;
+  console.log(data);
 
+  const { data: pacientesConsultas, isLoading: loadingConsultas } = useQuery({
+    queryKey: ["AgendamentosListInternal"],
+    queryFn: () => fetchAgendamentosList(idClinica, user.token),
+  });
+
+  console.log(pacientesConsultas);
+
+  const handleDelete = async () => {
+    if (window.confirm("Tem certeza que deseja deletar esta clínica?")) {
+      try {
+        await axios.delete(`${baseUrl}/clinics/${idClinica}/patients/${id}`, {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        });
+        navigate("..");
+      } catch (error) {
+        console.error("Erro ao deletar a clínica", error);
+      }
+    }
+  };
+
+  if (error) return <div>Error fetching clinics</div>;
+  if (isLoading || loadingConsultas) return <div>loading</div>;
   return (
-    <section className="flex-wrap gap-2 flex-col w-full flex items-center h-[83%]">
-      <h1>Paciente details</h1>
+    <section className="flex-wrap gap-2 flex-col w-full flex items-center ">
       <div className="flex flex-wrap gap-2">
         {isLoading ? (
           Array.from({ length: 6 }).map((_, index) => (
@@ -60,23 +84,135 @@ const DetailsPaciente = () => {
             </Card>
           ))
         ) : (
-          <Card key={data?.id}>
-            <CardHeader>
-              <CardTitle>{data?.name}</CardTitle>
-              <CardDescription>{data?.species}</CardDescription>
-            </CardHeader>
-            <CardContent className="flex flex-col">
-              <p>Breed: {data?.breed}</p>
-              <p>Guardião: {data?.guardian_name}</p>
-              <p>Contato do guardião: {data?.guardian_contact}</p>
-            </CardContent>
-            <CardFooter className="flex gap-2">
-              <Button onClick={() => navigate(`../editarPaciente/${id} `)}>
-                Editar
-              </Button>
-              <Button>Apagar</Button>
-            </CardFooter>
-          </Card>
+          <section>
+            <main className="flex-1 ">
+              <section className="w-full">
+                <div className="">
+                  <div className="grid gap-8 ">
+                    <div className="flex items-center justify-between">
+                      <h1 className="text-3xl font-bold">
+                        Detalhes do Paciente
+                      </h1>{" "}
+                      <div className="flex gap-5">
+                        <Button
+                          variant="outline"
+                          onClick={() => navigate(`../editarPaciente/${id} `)}
+                        >
+                          Editar
+                        </Button>
+                        <Button onClick={handleDelete} variant={"destructive"}>
+                          Apagar
+                        </Button>
+                      </div>
+                    </div>
+
+                    <Separator />
+                    <div className="grid md:grid-cols-2 gap-8 ">
+                      <div className=" p-6 rounded-lg shadow-md ">
+                        <div className="space-y-4">
+                          <div className="space-y-2">
+                            <h3 className="text-2xl font-bold">
+                              Informações Gerais
+                            </h3>
+                          </div>
+                          <div className="space-y-1">
+                            <div className="flex items-center justify-between">
+                              <span className="font-medium">Nome:</span>
+                              <span>{data?.name}</span>
+                            </div>
+                            <div className="flex items-center justify-between">
+                              <span className="font-medium">Espécie:</span>
+                              <span>{data?.species}</span>
+                            </div>
+
+                            <div className="flex items-center justify-between">
+                              <span className="font-medium">Idade:</span>
+                              <span>{data?.age}</span>
+                            </div>
+                            <div className="flex items-center justify-between">
+                              <span className="font-medium">
+                                Tipo sanguineo:
+                              </span>
+                              <span>{data?.breed}</span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="p-6 rounded-lg shadow-md">
+                        <div className="space-y-4">
+                          <div className="space-y-2">
+                            <h3 className="text-2xl font-bold">
+                              Informações do guardião
+                            </h3>
+                          </div>
+                          <div className="space-y-1">
+                            <div className="flex items-center justify-between">
+                              <span className="font-medium">Nome:</span>
+                              <span>{data?.guardian_name}</span>
+                            </div>
+                            <div className="flex items-center justify-between">
+                              <span className="font-medium">CPF:</span>
+                              <span>{data?.guardian_cpf}</span>
+                            </div>
+                            <div className="flex items-center justify-between">
+                              <span className="font-medium">Contato:</span>
+                              <span>{data?.guardian_contact}</span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className=" p-6 rounded-lg shadow-md">
+                      <div className="space-y-4">
+                        <div className="space-y-2">
+                          <h3 className="text-2xl font-bold">
+                            Histórico Clínico
+                          </h3>
+                        </div>
+                        <div className="space-y-1">
+                          {pacientesConsultas
+                            ?.filter((value) => value.patient_id === id)
+                            .map((item) => (
+                              <article className="flex gap-4 flex-col">
+                                <div>
+                                  <div className="flex items-center justify-between">
+                                    <span className="font-medium">
+                                      Data da consulta:
+                                    </span>
+                                    <span>
+                                      {dayjs(item.date).format(
+                                        "DD/MM/YYYY - HH:MM"
+                                      )}
+                                    </span>
+                                  </div>
+                                  <div className="flex items-center justify-between">
+                                    <span className="font-medium">
+                                      Serviço prestado
+                                    </span>
+                                    <span>{item.service.title}</span>
+                                  </div>
+                                  <div className="flex items-center justify-between">
+                                    <span className="font-medium">
+                                      Data de agendamento
+                                    </span>
+                                    <span>
+                                      {dayjs(item.created_at).format(
+                                        "DD/MM/YYYY - HH:MM"
+                                      )}
+                                    </span>
+                                  </div>
+                                </div>
+                                <Separator />
+                              </article>
+                            ))}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </main>
+          </section>
         )}
       </div>
     </section>
--- src/pages/DetailsServico/DetailsServico.tsx
+++ src/pages/DetailsServico/DetailsServico.tsx
@@ -3,46 +3,48 @@ import axios from "axios";
 import {
   Card,
   CardContent,
-  CardDescription,
-  CardFooter,
   CardHeader,
-  CardTitle,
+  CardFooter,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useNavigate, useParams } from "react-router-dom";
 import { useUserSelector } from "@/store/hooks";
 import { IService } from "@/interfaces/servico";
+import { fetchServiceItemList } from "@/Services/GetServices";
+import { Separator } from "@/components/ui/separator";
+import dayjs from "dayjs";
 
 const DetailsServico = () => {
   const baseUrl = import.meta.env.VITE_URL as string;
   const user = useUserSelector((state) => state.user);
   const { id, idClinica } = useParams();
   const navigate = useNavigate();
-  console.log(id, idClinica);
 
-  const fetchClinicasList = async (): Promise<IService> => {
-    const response = await axios.get(
-      `${baseUrl}/clinics/${idClinica}/services/${id}`,
-      {
-        headers: {
-          Authorization: `Bearer ${user.token}`,
-        },
+  const handleDelete = async () => {
+    if (window.confirm("Tem certeza que deseja deletar este serviço?")) {
+      try {
+        await axios.delete(`${baseUrl}/clinics/${idClinica}/services/${id}`, {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        });
+        navigate(`internalClinica/${idClinica}/dashboard`);
+      } catch (error) {
+        console.error("Erro ao deletar o serviço", error);
       }
-    );
-    return response.data;
+    }
   };
 
-  const { data, error, isLoading } = useQuery({
+  const { data, error, isLoading } = useQuery<IService>({
     queryKey: ["ServicoDetails"],
-    queryFn: fetchClinicasList,
+    queryFn: () => fetchServiceItemList(idClinica, id, user.token),
   });
 
-  if (error) return <div>Error fetching clinics</div>;
+  if (error) return <div>Error fetching service details</div>;
 
   return (
     <section className="flex-wrap gap-2 flex-col w-full flex items-center h-[83%]">
-      <h1>Service details</h1>
       <div className="flex flex-wrap gap-2">
         {isLoading ? (
           Array.from({ length: 6 }).map((_, index) => (
@@ -61,21 +63,65 @@ const DetailsServico = () => {
             </Card>
           ))
         ) : (
-          <Card key={data?.id}>
-            <CardHeader>
-              <CardTitle>{data?.title}</CardTitle>
-              <CardDescription>{data?.type}</CardDescription>
-            </CardHeader>
-            <CardContent className="flex flex-col">
-              <p>Quantidade: {data?.amount}</p>
-            </CardContent>
-            <CardFooter className="flex gap-2">
-              <Button onClick={() => navigate(`../editarServico/${id}`)}>
-                Editar
-              </Button>
-              <Button>Apagar</Button>
-            </CardFooter>
-          </Card>
+          <section>
+            <main className="flex-1">
+              <section className="w-full">
+                <div className="flex w-full flex-col gap-8">
+                  <div className="flex items-center justify-between">
+                    <h1 className="text-3xl font-bold">Detalhes do Serviço</h1>
+                    <div className="flex gap-5">
+                      <Button
+                        variant="outline"
+                        onClick={() => navigate(`../editarServico/${id}`)}
+                      >
+                        Editar
+                      </Button>
+                      <Button onClick={handleDelete} variant="destructive">
+                        Apagar
+                      </Button>
+                    </div>
+                  </div>
+
+                  <Separator />
+
+                  <div className="grid md:grid-cols-1 gap-8 min-w-[1000px] w-full">
+                    <div className="p-6 rounded-lg shadow-md">
+                      <div className="space-y-4">
+                        <div className="space-y-2">
+                          <h3 className="text-2xl font-bold">
+                            Informações do Serviço
+                          </h3>
+                        </div>
+                        <div className="space-y-1">
+                          <div className="flex items-center justify-between">
+                            <span className="font-medium">Título:</span>
+                            <span>{data?.title}</span>
+                          </div>
+                          <div className="flex items-center justify-between">
+                            <span className="font-medium">Tipo:</span>
+                            <span>{data?.type}</span>
+                          </div>
+                          <div className="flex items-center justify-between">
+                            <span className="font-medium">Preço:</span>
+                            <span>{data?.amount}</span>
+                          </div>
+
+                          <div className="flex items-center justify-between">
+                            <span className="font-medium">Criado em:</span>
+                            <span>
+                              {dayjs(data?.created_at).format(
+                                "DD/MM/YYYY - HH:mm"
+                              )}
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </main>
+          </section>
         )}
       </div>
     </section>
